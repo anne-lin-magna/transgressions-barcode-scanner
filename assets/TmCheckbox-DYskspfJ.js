f97645760bef617c273879e5eb3405e4859744f4
Anne Lin <100283618+anne-lin-magna@users.noreply.github.com>
2026-03-02 12:16:02 +0200

    Revert "Camera rotation test"
    This reverts commit 25dbe14216db24087990f5189763150496716998.

--- assets/TmCheckbox-DYskspfJ.js
+++
@@ -1,1 +0,0 @@
-import{a as t,j as a,v as e}from"./mui-Q5P_rhZz.js";import{a as m}from"./index-BpVB_2GP.js";const r=({testid:o,...s})=>a.jsx(e,{id:m(o),...s}),p=t.memo(r);export{p as T};
--- assets/TmIconButton-BllZxGbH.js
+++
@@ -1,1 +0,0 @@
-import{a as s,j as r,bb as m}from"./mui-Q5P_rhZz.js";const a=({testid:t,children:o,...n})=>r.jsx(m,{id:t,...n,children:o}),e=s.memo(a);export{e as T};
